89e49eeae942e422ec7b15b4b555a022afdf09a9
luxcgo <luxcgo@gmail.com>
2022-08-28 13:58:28 +0800

    controller/service: add http

--- docs/docs.go
+++ docs/docs.go
@@ -182,6 +182,53 @@ const docTemplate = `{
                 }
             }
         },
+        "/service/service_add_http": {
+            "post": {
+                "description": "添加HTTP服务",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "服务管理"
+                ],
+                "summary": "添加HTTP服务",
+                "operationId": "/service/service_add_http",
+                "parameters": [
+                    {
+                        "description": "body",
+                        "name": "body",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/dto.ServiceAddHTTPInput"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "success",
+                        "schema": {
+                            "allOf": [
+                                {
+                                    "$ref": "#/definitions/middleware.Response"
+                                },
+                                {
+                                    "type": "object",
+                                    "properties": {
+                                        "data": {
+                                            "type": "string"
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        },
         "/service/service_delete": {
             "get": {
                 "description": "服务删除",
@@ -349,6 +396,123 @@ const docTemplate = `{
                     "description": "密码",
                     "type": "string",
                     "example": "123456"
+                }
+            }
+        },
+        "dto.ServiceAddHTTPInput": {
+            "type": "object",
+            "required": [
+                "ip_list",
+                "rule",
+                "service_desc",
+                "service_name",
+                "weight_list"
+            ],
+            "properties": {
+                "black_list": {
+                    "description": "黑名单ip",
+                    "type": "string"
+                },
+                "clientip_flow_limit": {
+                    "description": "\u0008客户端ip限流",
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "header_transfor": {
+                    "description": "header转换",
+                    "type": "string"
+                },
+                "ip_list": {
+                    "description": "ip列表",
+                    "type": "string"
+                },
+                "need_https": {
+                    "description": "支持https",
+                    "type": "integer",
+                    "maximum": 1,
+                    "minimum": 0
+                },
+                "need_strip_uri": {
+                    "description": "启用strip_uri",
+                    "type": "integer",
+                    "maximum": 1,
+                    "minimum": 0
+                },
+                "need_websocket": {
+                    "description": "是否支持websocket",
+                    "type": "integer",
+                    "maximum": 1,
+                    "minimum": 0
+                },
+                "open_auth": {
+                    "description": "关键词",
+                    "type": "integer",
+                    "maximum": 1,
+                    "minimum": 0
+                },
+                "round_type": {
+                    "description": "轮询方式",
+                    "type": "integer",
+                    "maximum": 3,
+                    "minimum": 0
+                },
+                "rule": {
+                    "description": "域名或者前缀",
+                    "type": "string"
+                },
+                "rule_type": {
+                    "description": "接入类型",
+                    "type": "integer",
+                    "maximum": 1,
+                    "minimum": 0,
+                    "example": 0
+                },
+                "service_desc": {
+                    "description": "服务描述",
+                    "type": "string",
+                    "maxLength": 255,
+                    "minLength": 1
+                },
+                "service_flow_limit": {
+                    "description": "服务端限流",
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "service_name": {
+                    "description": "服务名",
+                    "type": "string"
+                },
+                "upstream_connect_timeout": {
+                    "description": "建立连接超时, 单位s",
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "upstream_header_timeout": {
+                    "description": "获取header超时, 单位s",
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "upstream_idle_timeout": {
+                    "description": "链接最大空闲时间, 单位s",
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "upstream_max_idle": {
+                    "description": "最大空闲链接数",
+                    "type": "integer",
+                    "minimum": 0
+                },
+                "url_rewrite": {
+                    "description": "url重写功能",
+                    "type": "string"
+                },
+                "weight_list": {
+                    "description": "\u0008权重列表",
+                    "type": "string"
+                },
+                "white_list": {
+                    "description": "白名单ip",
+                    "type": "string"
                 }
             }
         },
--- dto/admin_login.go
+++ dto/admin_login.go
@@ -8,7 +8,7 @@ import (
 )
 
 type AdminLoginInput struct {
-	Username string `form:"username" json:"username" comment:"用户名"  validate:"required,is_valid_username" example:"admin"`
+	Username string `form:"username" json:"username" comment:"用户名"  validate:"required,valid_username" example:"admin"`
 	Password string `form:"password" json:"password" comment:"密码"   validate:"required" example:"123456"`
 }
 
